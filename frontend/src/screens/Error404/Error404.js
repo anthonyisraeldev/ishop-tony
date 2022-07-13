b9f47e1bcd4f10bc0c26d7c2e922468092144dc1
Anthony Teran <anthonyisrael.dev@gmail.com>
2022-07-13 19:43:22 -0400

    Screen 404 updated

--- frontend/src/screens/Error404/Error404.js
+++ frontend/src/screens/Error404/Error404.js
@@ -9,10 +9,12 @@ const Error404 = () => {
     <div>
       <div className="content-box content-not-found">
         <h2 className="main-title">Oops!</h2>
-        <p className="text-description">La página que busca no se encuentra</p>
+        <p className="text-description">
+          You just hit a route that doesn't exist... the sadness. 🥺
+        </p>
         <img src={notFound} className="img-notfound" alt="404 Page not found" />
         <Link to="/" className="btn btn-primary" title="Ir a inicio">
-          Ir a inicio <FaHome />
+          Go home <FaHome />
         </Link>
       </div>
     </div>
